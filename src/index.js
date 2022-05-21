31ff0343907ab5736e5076eb5be823b5bc72e343
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2022-05-21 16:01:48 +0300

    fix router

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ console.log('gdhkjghdkh')
 const router = Router.instance();
 
 router
-  .addRoute(/^Beat-Blade\/?$/, 'game')
+  .addRoute(/Beat-Blade\/?$/, 'game')
   .addRoute(/^404\/?$/, 'error404')
   .setNotFoundPagePath('error404')
   .listen();
